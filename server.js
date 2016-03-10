81d796e667e2e9521f0290ff9948c369f1104094
Klim Shumaiev <klimksh@gmail.com>
2016-03-10 13:51:28 +0100

    server refactoring + cors added

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Movie = require('./models/movie');
 var movieController = require('./controllers/movie');
+var cors = require('cors');
 
 // Connect to the MongoDB with movies
 mongoose.connect('mongodb://localhost:27017/moviedb');
 
 // Create our Express application
 var app = express();
-
+app.use(cors());
 // Use the body-parser package in our application
 app.use(bodyParser.urlencoded({
     extended: true
@@ -21,12 +22,6 @@ var port = process.env.PORT || 3000;
 
 // Create our Express router
 var router = express.Router();
-
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-    res.json({ message: 'You are running dangerously low on beer!' });
-});
 
 // Register all our routes with /api
 app.use('/api', router);
@@ -41,9 +36,6 @@ router.get('/', function(req, res) {
     res.json({ message: 'You are running dangerously low on beer!' });
 });
 
-// -- New Code Below Here -- //
-
-// Create a new route with the prefix /movies
 router.route('/movies')
     .post(movieController.postMovie)
     .get(movieController.getMovies);
