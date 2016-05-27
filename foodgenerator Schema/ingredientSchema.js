c5df2e0ac6af4c8e1fdcfa4822e6aa360479122d
LordNorbi <roberthaecker@web.de>
2016-05-27 16:27:51 +0200

    Schema Update 2.0

--- foodgenerator Schema/ingredientSchema.js
+++ foodgenerator Schema/ingredientSchema.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 // Define our movie schema
 var Ingredient   = new mongoose.Schema({
     title: String,
+    supermarket:[String],
+    picture: String
 });
 
 // Export the Mongoose model
--- foodgenerator Schema/recipeFamilySchema.js
+++ foodgenerator Schema/recipeFamilySchema.js
@@ -5,17 +5,8 @@ var mongoose = require('mongoose');
 var RecipeFamily   = new mongoose.Schema({
     defaultrecipe: {type: mongoose.Schema.Type.ObjectId,
 		  ref:'recipe'}
-    recipes: [{type: mongoose.Schema.Type.ObjectId,
-		  ref:'recipe'}]
 });
 
-
-RecipeFamily.methods.getDefaultRecipe = function (){
-	return this.defaultrecipe;
-}
-RecipeFamily.methods.getFamilyRecipes = function (){
-	return this.recipes;
-}
 
 // Export the Mongoose model
 module.exports = mongoose.model('RecipeFamily', recipeFamilySchema);
