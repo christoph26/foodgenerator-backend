c1b129dfbd838b1e1a16ac65565cfc64168b5814
Simon Bönisch <git@sunborn.me>
2016-05-30 15:25:10 +0200

    added automated comment removal, finished first implementation of json-importing database initializer

--- database/initDbWithTestData.js
+++ database/initDbWithTestData.js
@@ -2,10 +2,28 @@
  * Development script for manual database initialization and data import
  */
 
+const DATA_FOLDER_PATH = "../dummydata/";
+const FILE_PATH_INGREDIENT = "Ingredient.json";
+const FILE_PATH_RECIPE = "recipe.json";
+const FILE_PATH_SUPERMARKET = "supermarket.json";
+const FILE_PATH_USER = "user.json";
+
+var UglifyJS = require("uglify-js");
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
+function removeCommentsAndOpen(directoryPath, fileName) {
+    var fileContent = fs.readFileSync(directoryPath + fileName, "utf8");
+    var abstractSyntaxTree = UglifyJS.parse(fileContent, {filename: fileName});
+    var outputStream = UglifyJS.OutputStream({quote_keys: true, semicolons: false});
+    abstractSyntaxTree.print(outputStream);
+    var code = outputStream.toString();
+    return JSON.parse(code);
+}
+
 // connect to running mongoDb instance on localhost and default port
-var mongoClient = require('mongodb').MongoClient;
 console.log("Connecting to database at localhost:27017...");
-mongoClient.connect("mongodb://localhost:27017/foodgeneratorDb", function (err, db) {
+MongoClient.connect("mongodb://localhost:27017/foodgeneratorDb", function (err, db) {
     // in case of error, print it to console and exit
     if (err) {
         return console.dir(err);
@@ -22,46 +40,29 @@ mongoClient.connect("mongodb://localhost:27017/foodgeneratorDb", function (err, 
     db.createCollection("supermarkets");
     console.log("Collections created.");
 
+    console.log("Starting to import entities.");
     // import the supermarket entities
-    var testSupermarket = {
-        titel: "Testtitel",
-        icon: "test_link_to_pic"
-    };
-    var result = db.supermarkets.insert(testSupermarket);
+    var supermarkets = removeCommentsAndOpen(DATA_FOLDER_PATH, FILE_PATH_SUPERMARKET);
+    var result = db.collection("supermarkets").insertMany(supermarkets);
     console.log(result);
 
     // import the ingredient entities
-    var testIngredient = {
-        title: "Testtitel",
-        supermarket: ["Testsupermarkt1", "Testsupermarkt2"],
-        picture: "relative_path_to_pic"
-    };
-    result = db.ingredients.insert(testIngredient);
+    var ingredients = removeCommentsAndOpen(DATA_FOLDER_PATH, FILE_PATH_INGREDIENT);
+    result = db.collection("ingredients").insertMany(ingredients);
     console.log(result);
 
     // import the recipe entities
-    var testRecipe = {
-        title: "Testtitle",
-        skill: 2,
-        description: "Testdescription",
-        vegan: false,
-        vegetarian: true,
-        ingredientlist: {
-            amount: [],
-            ingredient: []
-        },
-        recipeFamily: {}
-    };
-    result = db.recipes.insert(testRecipe);
+    var recipes = removeCommentsAndOpen(DATA_FOLDER_PATH, FILE_PATH_RECIPE);
+    result = db.collection("recipes").insertMany(recipes);
     console.log(result);
 
-    // import the recipe family entities
-    var testRecipeFamily = {
-        defaultrecipe: {}
-    };
-    result = db.recipefamilies.insert(testRecipeFamily);
-    console.log(result);
-
+    // // import the recipe family entities
+    // var recipeFamilies = removeCommentsAndOpen(DATA_FOLDER_PATH, FILE_PATH_RECIPE_FAMILY);
+    // result = db.collection("recipefamilies").insertMany(recipeFamilies);
+    // console.log(result);
+    
     // signalize success
+    db.close();
     console.log("All tasks executed without errors.");
 });
+
