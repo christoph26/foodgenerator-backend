8a8cc2ea677d4763dba3aae8d1cfc59bd690189e
Simon Bönisch <git@sunborn.me>
2016-05-30 13:51:49 +0200

    rewrote script to make use of mongodb lib instead of the mongo shell's objects

--- database/initDbWithTestData.js
+++ database/initDbWithTestData.js
@@ -1,61 +1,63 @@
 /**
- * Created by christoph on 29.05.16.
+ * Development script for manual database initialization and data import
  */
 
-//Connect to running mongoDb instance on localhost and default port
-conn = new Mongo();
-db = conn.getDB("foodgeneratorDb");
-var result;
+// connect to running mongoDb instance on localhost and default port
+var mongoClient = require('mongodb').MongoClient;
+mongoClient.connect("mongodb://localhost:27017/foodgeneratorDb", function (err, db) {
+    // in case of error, print it to console and exit
+    if (err) {
+        return console.dir(err);
+    }
 
-//delete all data
-//db.getCollectionNames().forEach(
-//    function(collection_name) {
-//        db[collection_name].remove({})
-//    }
-//);
-db.dropDatabase();
-print("Data deleted.");
+    // delete the database before recreating it
+    db.dropDatabase();
+    console.log("Old data deleted.");
 
+    // create the empty collections
+    db.createCollection("ingredients");
+    db.createCollection("recipes");
+    db.createCollection("recipefamilies");
+    db.createCollection("supermarkets");
+    console.log("Collections created.");
 
-//create collections
-db.createCollection("ingredients");
-db.createCollection("recipes");
-db.createCollection("recipefamilies");
-db.createCollection("supermarkets");
-print("Collections created.");
+    // import the supermarket entities
+    var testSupermarket = {
+        titel: "Testtitel",
+        icon: "test_link_to_pic"
+    };
+    var result = db.supermarkets.insert(testSupermarket);
+    console.log(result);
 
-var testIngredient = {
-    title: "Testtitel",
-    supermarket: ["Testsupermarkt1", "Testsupermarkt2"],
-    picture: "relative_path_to_pic"
-};
-result = db.ingredients.insert(testIngredient);
-print(result);
+    // import the ingredient entities
+    var testIngredient = {
+        title: "Testtitel",
+        supermarket: ["Testsupermarkt1", "Testsupermarkt2"],
+        picture: "relative_path_to_pic"
+    };
+    result = db.ingredients.insert(testIngredient);
+    console.log(result);
 
-var testRecipe = {
-    title: "Testtitle",
-    skill: 2,
-    description: "Testdescription",
-    vegan: false,
-    vegetarian: true,
-    ingredientlist: {
-        amount: [],
-        ingredient: []
-    },
-    recipeFamily: {}
-};
-result = db.recipes.insert(testRecipe);
-print(result);
+    // import the recipe entities
+    var testRecipe = {
+        title: "Testtitle",
+        skill: 2,
+        description: "Testdescription",
+        vegan: false,
+        vegetarian: true,
+        ingredientlist: {
+            amount: [],
+            ingredient: []
+        },
+        recipeFamily: {}
+    };
+    result = db.recipes.insert(testRecipe);
+    console.log(result);
 
-var testRecipeFamily = {
-    defaultrecipe: {}
-};
-result = db.recipefamilies.insert(testRecipeFamily);
-print(result);
-
-var testSupermarket = {
-    titel: "Testtitel",
-    icon: "test_link_to_pic"
-};
-result = db.supermarkets.insert(testSupermarket);
-print(result);
+    // import the recipe family entities
+    var testRecipeFamily = {
+        defaultrecipe: {}
+    };
+    result = db.recipefamilies.insert(testRecipeFamily);
+    console.log(result);
+});
