79a0c2ec85225306f453ac595b6410d24a0e2ae6
Norbi <roberthaecker@web.de>
2016-06-02 13:00:21 +0200

    user GET , POST, DELETE 5.1 comments updated

--- components/user/userController.js
+++ components/user/userController.js
@@ -3,7 +3,7 @@ var User = require('./userSchema');
 var jwt = require('jwt-simple');
 
 module.exports.login = function(req, res){
-
+//check for pw and email
     if (!req.body.email) {
         res.status(400).send('username required');
         return;
@@ -12,21 +12,23 @@ module.exports.login = function(req, res){
         res.status(400).send('password required');
         return;
     }
-
+//search user by email
     User.findOne({email: req.body.email}, function (err, user) {
         if (err) {
             res.status(500).send(err);
             return
         }
-
+//if no user found, return
         if (!user) {
             res.status(401).send('Invalid Credentials');
             return;
         }
+        //check for right pw
         user.comparePassword(req.body.password, function(err, isMatch) {
             if(!isMatch || err){
                 res.status(401).send('Invalid Credentials');
             } else {
+                //if right: creates token
                 res.status(200).json({token: createToken(user)});
             }
         });
@@ -35,6 +37,7 @@ module.exports.login = function(req, res){
 };
 
 module.exports.signup = function(req, res){
+    //check for required information
     if(!req.body.email){
         res.status(400).send('email required');
         return;
@@ -51,7 +54,7 @@ module.exports.signup = function(req, res){
         res.status(400).send('last name required');
         return;
     }
-
+//create new user, insert information
     var user = new User();
     user.email = req.body.email;
     user.password = req.body.password;
@@ -59,20 +62,20 @@ module.exports.signup = function(req, res){
     user.lastName = req.body.lastName;
     user.salt = "salzig";
 
-
+//safe user in db
     user.save(function(err) {
         if (err) {
             res.status(500).send(err);
             return;
         }
-
+//creates token
         res.status(201).json({token: createToken(user)});
     });
 };
 
 //deletes an user
 module.exports.unregister = function(req, res) {
-
+//check fpr emaila nd pw
     if (!req.body.email) {
         res.status(400).send('username required');
         return;
@@ -81,7 +84,7 @@ module.exports.unregister = function(req, res) {
         res.status(400).send('password required');
         return;
     }
-
+//search for email in db
     User.findOne({email: req.body.email}, function (err, user) {
         if (err) {
             res.status(500).send(err);
@@ -92,10 +95,12 @@ module.exports.unregister = function(req, res) {
             res.status(401).send('Invalid Credentials');
             return;
         }
+        //check if right pw
         user.comparePassword(req.body.password, function(err, isMatch) {
             if(!isMatch || err){
                 res.status(401).send('Invalid Credentials');
             } else {
+                //if right pw, delete user
                 user.remove();
                 res.status(400).send('user deleted');
             }
