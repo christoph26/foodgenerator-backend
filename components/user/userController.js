0ba5bf8c4abc2db0530eac07930e6ea3f117d558
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-05 13:19:05 +0200

    removed Json conversion, as methods succeed when content-type is given

--- components/user/userController.js
+++ components/user/userController.js
@@ -41,79 +41,75 @@ module.exports.login = function (req, res) {
 };
 
 module.exports.signup = function (req, res) {
-    req.on("data", function (body) {
-        body = JSON.parse(body.toString());
-        //check for required information
-        if (!body.email) {
-            res.status(400).send('email required');
-            return;
-        }
-        if (!body.password) {
-            res.status(400).send('password required');
-            return;
-        }
-        if (!body.firstName) {
-            res.status(400).send('first name required');
-            return;
-        }
-        if (!body.lastName) {
-            res.status(400).send('last name required');
-            return;
-        }
-        //create new user object, insert information
-        var user = new User();
-        user.email = body.email;
-        user.password = body.password;
-        user.firstName = body.firstName;
-        user.lastName = body.lastName;
-        user.salt = "salzig";
+    var body = req.body;
+    //check for required information
+    if (!body.email) {
+        res.status(400).send('email required');
+        return;
+    }
+    if (!body.password) {
+        res.status(400).send('password required');
+        return;
+    }
+    if (!body.firstName) {
+        res.status(400).send('first name required');
+        return;
+    }
+    if (!body.lastName) {
+        res.status(400).send('last name required');
+        return;
+    }
+    //create new user object, insert information
+    var user = new User();
+    user.email = body.email;
+    user.password = body.password;
+    user.firstName = body.firstName;
+    user.lastName = body.lastName;
+    user.salt = "salzig";
 
-        //safe user in db
-        user.save(function (err) {
-            if (err) {
-                res.status(500).send(err);
-                return;
-            }
-            //return user
-            res.status(201).json(user);     //FIXME remove password before transmitting user
-        });
+    //safe user in db
+    user.save(function (err) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+        //return user
+        res.status(201).json(user);     //FIXME remove password before transmitting user
     });
 };
 
 //deletes a user
 module.exports.unregister = function (req, res) {
-    req.on("data", function (body) {
-        body = JSON.parse(body.toString());
-        //check for email and pw
-        if (!body.email) {
-            res.status(400).send('username required');
-            return;
+    var body = req.body;
+    //check for email and pw
+    if (!body.email) {
+        res.status(400).send('username required');
+        return;
+    }
+    if (!body.password) {
+        res.status(400).send('password required');
+        return;
+    }
+    //search for email in db
+    User.findOne({email: body.email}, function (err, user) {
+        if (err) {
+            res.status(500).send(err);
+            return
         }
-        if (!body.password) {
-            res.status(400).send('password required');
-            return;
-        }
-        //search for email in db
-        User.findOne({email: body.email}, function (err, user) {
-            if (err) {
-                res.status(500).send(err);
-                return
-            }
 
-            if (!user) {
+        if (!user) {
+            res.status(401).send('Invalid Credentials');
+            return;
+        }
+        //check if right pw
+        user.comparePassword(body.password, function (err, isMatch) {
+            if (!isMatch || err) {
                 res.status(401).send('Invalid Credentials');
-                return;
+            } else {
+                //if right pw, delete user
+                user.remove();
+                res.status(200).send('user deleted');
             }
-            //check if right pw
-            user.comparePassword(body.password, function (err, isMatch) {
-                if (!isMatch || err) {
-                    res.status(401).send('Invalid Credentials');
-                } else {
-                    //if right pw, delete user
-                    user.remove();
-                    res.status(200).send('user deleted');
-                }
-            });
         });
     });
 };
