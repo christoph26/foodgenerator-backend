18f6b175754ef3ba622766e3bfc22d8b752d5b42
christoph26 <dehner@in.tum.de>
2016-06-15 18:53:08 +0200

    Fixed Get

--- components/mealPlan/mealPlanRoutes.js
+++ components/mealPlan/mealPlanRoutes.js
@@ -5,8 +5,8 @@ function mealPlanRoutes() {
     var mealPlanController = require('./mealPlanController');
     var router = require('express').Router();
 
-    //router.route('/recipes/:id')
-    //    .get(mealPlanController.getMealplan);
+    router.route('/mealPlans/:id')
+        .get(mealPlanController.getMealPlan);
 
     router.route('/mealPlans/')
         .post(mealPlanController.createMealPlan);
