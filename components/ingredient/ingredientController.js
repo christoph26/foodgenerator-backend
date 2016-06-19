6c8652c067f8245b0628d14b7fcec42fbfab3bf8
christoph26 <dehner@in.tum.de>
2016-06-19 13:17:37 +0200

    Ingredient autocomplete

--- components/ingredient/ingredientController.js
+++ components/ingredient/ingredientController.js
@@ -1,5 +1,23 @@
 var Ingredient = require('./ingredientSchema');
 var base = require('../base');
 
-// Create endpoint /api/movies/:id for GET
 exports.getIngredient = base.getEntityById(Ingredient)
+
+exports.findIngredientAutocomplete = function (req, res) {
+    var searchTerm = req.body.searchTerm;
+    var regex = new RegExp(searchTerm, 'i');
+
+    Ingredient.find({title: regex}).select("title _id").lean().exec(function (err, result) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        for (var i = 0; i < result.length; i++) {
+            result[i].text = result[i].title;
+        }
+        res.json(result);
+    });
+
+
+}
