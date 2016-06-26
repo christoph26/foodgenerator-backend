3c19ea0218a4241336a40d7c449a17eea7f43075
Simon Bönisch <simonboenisch@yahoo.de>
2016-06-26 15:44:28 +0200

    implemented list all meal plans resource

--- components/mealPlan/mealPlanController.js
+++ components/mealPlan/mealPlanController.js
@@ -1,6 +1,8 @@
 var MealPlan = require('./mealPlanSchema');
 var base = require('../base');
 
+var async = require("async");
+
 // Create endpoint /users/:id for GET
 exports.getMealPlan = function (req, res) {
     MealPlan.findById(req.params.id).lean().exec(function (err, result) {
@@ -113,6 +115,31 @@ module.exports.deleteMealPlan = function (req, res) {
 
         }
     });
+};
 
+module.exports.listMealPlans = function (req, res) {
+    var body = req.body;
+
+    // Check authorisation
+    var idFromToken = base.getIdFromToken(req.headers['authorization'].split(" ")[1]);
+    if (!idFromToken || !(idFromToken.user._id == body.user)) {
+        res.status(403).send('Unauthorized!');
+        return;
+    }
+
+    // Assemble query
+    var query = MealPlan.find();
+    query.where("user", idFromToken.user);
+    query.select("title _id");
+
+    // Execute query and return result
+    query.lean().exec(function (queryError, queryResult) {
+        if (queryError) {
+            res.status(500).send(queryError);
+            return
+        }
+
+        res.json(queryResult);
+    })
 
 };
